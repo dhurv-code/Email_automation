ed7d8598c2b94ff16c761ac3ca7f1c524b05362a
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-06-19 14:06:48 +0530

    updation

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,20 @@ from app.api.campaigns import router as campaign_router
 from app.api.conversations import router as conversations_router
 from app.api.dashboard import router as dashboard_router
 from app.api.email_logs import router as email_logs_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     gmail_router,prefix="/gmail",tags=["gmail"]
